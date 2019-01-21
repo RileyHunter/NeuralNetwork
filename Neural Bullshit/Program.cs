2655b6423c0ec184bb0e4cff25e16f03755a0757
Riley Hunter <rileyh@imagimationschool.com>
2019-01-22 12:47:01 +1300

    Comments for Main

--- Neural Bullshit/Program.cs
+++ Neural Bullshit/Program.cs
@@ -12,11 +12,18 @@ namespace NeuralNetwork
 {
     class Program
     {
+        // Currently, this program creates a 3-layer network with 10 hidden nodes.
+        // The network accepts binary training data to learn [A XOR B] and ignore input C.
+        // With this structure and about 2000+ training epochs, the network slowly learns
+        // to perform binary logic, and scores full accuracy on the training data.
+        // In principle, there's nothing stopping this network from being used for all 
+        // sorts of fun new data analysis now.
         static void Main(string[] args)
         {
+            //Create a new 3-10-1 NN
             var network = new Structure.NeuralNetwork(new [] { 3, 10, 1 });
 
-
+            //Init. training data and labels
             var trainingData = new[]
             {
                 new [] { BinaryLow, BinaryLow, BinaryLow },
@@ -31,21 +38,24 @@ namespace NeuralNetwork
 
             var labels = new[]
             {
-                new [] { BinaryLow },
-                new [] { BinaryHigh },
-                new [] { BinaryHigh },
-                new [] { BinaryLow },
-                new [] { BinaryLow },
-                new [] { BinaryHigh },
-                new [] { BinaryHigh },
-                new [] { BinaryLow }
+                new [] { 0d },
+                new [] { 1d },
+                new [] { 1d },
+                new [] { 0d },
+                new [] { 0d },
+                new [] { 1d },
+                new [] { 1d },
+                new [] { 0d }
             };
 
+            //TrainConfig is a class used to store important training hyperparameters
             var myTrainConfig = new TrainConfig();
             myTrainConfig.Epochs = 5000;
+
+            //Train the network using the config, network and data above
             Tasks.Train(network, trainingData, labels, myTrainConfig);
             
-
+            //Without further training, feed each data sample through the network and output to console
             foreach (var data in trainingData)
             {
                 network.InputLayer.FeedForward(data);
@@ -55,6 +65,7 @@ namespace NeuralNetwork
                 Console.WriteLine(output);
             }
 
+            //Use the prebuilt regression accuracy task to get an avg error.
             Console.WriteLine("Avg error: " + Tasks.TestRegressionAccuracy(network, trainingData, labels));
             Console.ReadKey();
         }
